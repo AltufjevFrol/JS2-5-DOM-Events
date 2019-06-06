0bac89580a72fa31e50a5956f10b674c91792a49
Frol <artem.mihailovich@gmail.com>
2019-06-06 04:48:30 +0300

    Finished

--- script.js
+++ script.js
@@ -1,29 +1,106 @@
 'use strict';
 
 // Код валидации формы
-function validateForm () {
+function validateForm (settings) {
+/*
+*получаем форму которую необходимо проверять
+*/
+var form = document.getElementById(settings.formId);
 
+/*
+*слушаем на ней события
+*/
+form.addEventListener('blur', handlerBlur, true)
+form.addEventListener('focus', handlerFocus, true)
+form.addEventListener('submit', handlerSubmit)
+
+function handlerBlur (event) {
+if (event.target.tagName === 'INPUT') {
+if (!checkInput(event.target)) {//если проверка не пройдена
+	event.target.classList.add(settings.inputErrorClass)
+}
+}
 }
 
-function handler(event) {
+function checkInput(input){// возвращает true или false
+	var empty = true;
+	var leter = true;
+	var number = true;
+	var phone = true;
+
+	if (input.dataset.required){
+		//проверка на заполнение
+		if (input.value.length === 0){return false}
+	}
+
+	if (input.dataset.validator){
+		//проверка в зависимости от типа валидатора
+
+		if (input.dataset.validator === 'letters'){
+			//провкрка на текст
+				if (input.value.length === 0){return false}
+				var reg=/^[а-яА-ЯёЁa-zA-Z]+$/;
+				leter = reg.test(input.value);
+			}
+			
+		if (input.dataset.validator === 'number'){
+			//проверка на цыфры
+			if (input.value.length === 0){return false}
+			var reg = /^[0-9]+$/;
+			var ofNum = reg.test(input.value) 
+			var max = true;
+			var min = true;
+			if(input.dataset.validatorMin){
+				max = +input.dataset.validatorMin <= input.value;
+			}
+			if (input.dataset.validatorMax) {
+				min = input.value <= +input.dataset.validatorMax;
+			}
+			number = ofNum && max && min;
+		}
+			if (input.dataset.validator === 'regexp'){
+				//проверка на шаблон
+			if (input.value.length === 0){return false}
+				phone = new RegExp(input.dataset.validatorPattern).test(input.value)
+			}
+
+	}
+
+	return empty && leter && number && phone;
+}
+
+function handlerFocus(event){
+	if(event.target.tagName === 'INPUT'){
+	event.target.classList.remove('input_error');
+	}
+}
+
+function handlerSubmit (event){
 	event.preventDefault();
-this.setAttribute('style', 'background: red');
-console.log(event);
+	var self = this;
+	var inputs = Array.from(document.querySelectorAll('input'));
+	//проверили импуты
+	inputs.forEach(function(input){
+		if (!checkInput(input)){
+			input.classList.add(settings.inputErrorClass)
+		}
+	});
+// посчитали ошибочные
+	var countErr = 0;
+	inputs.forEach(function(input){
+		if (input.classList.contains(settings.inputErrorClass)){
+			countErr++;
+		}
+	});
+// ошибки есть? ошибок нет!
+	if (countErr>0){
+		self.classList.remove(settings.formValidClass);
+		self.classList.add(settings.formInvalidClass);
+	} else {
+		self.classList.remove(settings.formInvalidClass);
+		self.classList.add(settings.formValidClass);
+	}
+
 }
 
-function handler2(event) {
-	event.preventDefault();
-event.target.setAttribute('style', 'background: red');
-console.log(event);
 }
-
-var button = document.querySelector('button');
-var body = document.querySelector('body');
-var form = document.querySelector('form');
-var input = document.querySelector('#profile-age');
-console.log(button);
-console.log(body);
-console.log(form);
-
-body.addEventListener('submit', handler);
-body.addEventListener('blur', handler2, true)
